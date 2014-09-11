960273bd205dc6d1ef4d959e9a70196e2dac4b7d
MattSainz <Matthias.Sainz@colorado.edu>
2014-09-11 17:01:06 -0600

    Better comments

--- app/models/data_sets.rb
+++ app/models/data_sets.rb
@@ -1,16 +1,22 @@
 class DataSets < ActiveRecord::Base
   validates :Name , uniqueness: true
+    #ensures only unique entries are added to the db
 
-  def xml_u
+  def xml_upload
     puts "Checking for new database entries"
 
     xml_data = File.open("#{Rails.root}/db/DataSetsExcel.xml")
     doc = Nokogiri::XML(xml_data)
-    xml_data_sets = doc.xpath("//DataSet")
 
-    xml_data_sets.each do |data_set|
+    xml_data_entires = doc.xpath("//DataSet")
+      #Gets all the individual <dataset> in the xml file
+
+    xml_data_entires.each do |data_set|
+      #iterate through all of the dataset entries in the
       to_database = DataSets.new
+        #make a new db entry
       data_set.children.each do |element|
+        #iterate through all of the entry attributes i.e Name
         case element.name
           when 'Name'
             to_database.Name = element.text
@@ -50,7 +56,7 @@ class DataSets < ActiveRecord::Base
       end
       #validates before every save
       to_database.save
+        #save to db
     end
   end
-
 end
