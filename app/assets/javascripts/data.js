e462ae5a6d1b07feed64ddbb2327164882112b6b
MattSainz <Matthias.Sainz@colorado.edu>
2014-09-30 16:43:52 -0600

    Added auto ugly colors ( the auto part was the accomplishment) and fixed elements to say inside containers no matter size

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,15 +1,18 @@
 $(document).on("page:change", function(){
-    function equalHeight(obj){
-        var heightArray = obj.children("div").map( function(){
-            return  $(this).height();
-        }).get();
-        var maxHeight = Math.max.apply( Math, heightArray);
-        obj.children("div").height(maxHeight);
-    }
 
-    $(".row").each( function(){
-        equalHeight( $(this));
-    })
+  function equalHeight(obj){
+    var heightArray = obj.children("div").map( function(){
+      return  $(this).height();
+    }).get();
+    var maxHeight = Math.max.apply( Math, heightArray);
+    obj.children("div").height(maxHeight);
+  }
+
+  $(".row").each( function(){
+    equalHeight( $(this));
+  })
+
+ // $(".large-6").height(30);
 
 });
 
--- app/helpers/entry_builder.rb
+++ app/helpers/entry_builder.rb
@@ -1,2 +1,83 @@
-class EntryBuilder
+module EntryBuilder
+
+  Entry     = Struct.new(:infoEntry,:additionalEntries)
+  AttrColor = Struct.new(:attr, :color)
+  Button    = Struct.new(:state, :label, :link)
+
+  @colors = { "social" => "blue", "biological" => "green", "informational" => "orange"}
+
+  def self.get_entries(search_query)
+     if search_query.nil?
+       my_q = search_query
+     else
+       my_q = "name NOT NULL"
+     end
+     return process_entries( DataSets.get_groups(my_q) )
+  end
+
+  def self.process_entries(data)
+     to_ret = []
+       #collection of formatted entries to be rendered by the view
+     data.each_with_index do |group|
+        #determin if entry is part of a group or not
+        if group.length == 1
+          to_ret.append( Entry.new( format_entry(group[0]) , NIL) )
+            #add the single entry to the list
+        else
+          f_group = []
+          group.each_with_index do | entry |
+            f_group.append( format_entry( entry ) )
+          end
+          to_ret.append( Entry.new( f_group.pop, f_group ))
+            #TODO find a better entry to represent the entire group i.e. parse all entries and find
+            #what is different about them
+        end
+     end
+     return to_ret
+  end
+
+  def self.format_entry(e)
+    proper_entry = { :Name => e.Name }
+    proper_entry[:Domain]    = AttrColor.new(e.Domain,@colors[e.Domain.downcase])
+    proper_entry[:SubDomain] = AttrColor.new(e.SubDomain,(!@colors[e.Domain.downcase].nil?) ? "light" + @colors[e.Domain.downcase] : "")
+    proper_entry[:Nodes]     = e.Nodes
+    proper_entry[:Edges]     = e.Edges
+
+    description = e.Description
+    if e.NodeType.length > 15
+      description += " \n Node Type: " + e.NodeType
+      proper_entry[:NodeType] = "See Description"
+    else
+      proper_entry[:NodeType] = e.NodeType
+    end
+
+    if e.EdgeType.length > 15
+      description += " \n Edge Type: " + e.EdgeType
+      proper_entry[:EdgeType] = "See Description"
+    else
+      proper_entry[:EdgeType] = e.EdgeType
+    end
+    proper_entry[:Description] = description
+
+    proper_entry[:GraphProperties] = e.GraphProperties # TODO parse and color
+    proper_entry[:GroupId]         = e.GroupId
+    proper_entry[:InfoLink]        = e.InfoLink
+    proper_entry[:FileSize]        = e.FileSize
+    proper_entry[:FileType]        = e.FileType
+    proper_entry[:GraphFormat]     = e.GraphFormat
+    proper_entry[:Citation]        = e.Citation
+
+    if e.Public == true
+      proper_entry[:DataLink] = AttrColor.new(e.DataLink,"gray")
+    else
+      proper_entry[:DataLink] = AttrColor.new(e.DataLink,"blue")
+    end
+
+    return proper_entry
+  end
 end
+
+   # Check if there is more than one entry in a group
+   # if there is change the download link
+   # check if it is public or not and then effect the download link
+   # make a mapping of names to colors
