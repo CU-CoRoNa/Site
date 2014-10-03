3388db7e26f15331e7a7c0ad0174f4a885ec3c34
MattSainz <Matthias.Sainz@colorado.edu>
2014-10-02 20:31:52 -0600

    Description and attribute area resizing now works

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,23 +1,51 @@
+//=require jquery.expander.js
+var collapsed_height = 30;
+
 $(document).on("page:change", function(){
 
-  function equalHeight(obj){
-    var heightArray = obj.children("div").map( function(){
-      return  $(this).height();
-    }).get();
-    var maxHeight = Math.max.apply( Math, heightArray);
-    obj.children("div").height(maxHeight);
-  }
+  $(".collapse").height(collapsed_height);
 
-  $(".row").each( function(){
-    equalHeight( $(this));
-  })
+});
 
- // $(".large-6").height(30);
+$('.collapse').on('click',function(event){
+    showHide($(this));
+});
 
+$('.collapse').hover(function(){
+   $(this).css({'background-color' : '#F0F0F0'});
+},function(){
+   $(this).css({'background-color': '#F7F7F7'});
 });
 
 function showMore(arg)
 {
   $("#" + arg).slideToggle(500);
-}
+}//end showMore
+
+function showHide(elem)
+{
+  var old_height = elem.height();
+  elem.css({'height': 'auto'});
+  heights = [elem.height()];
+  elem.siblings().each(function(){
+    $(this).css({'height':'auto'});
+    heights.push($(this).height());
+  });
+
+  var height_m = Math.max.apply(Math,heights);
+
+  if(old_height == 30)
+  {
+    console.log("Make Bigger");
+    elem.height(height_m);
+    elem.siblings().height(height_m);
+  }
+  else
+  {
+    console.log("Make smaller");
+    elem.height(collapsed_height);
+    elem.siblings().height(collapsed_height);
+  }
+
+}//end showMore
 
